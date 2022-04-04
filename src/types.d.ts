a0a52b60251ae20532d38830268f24e829388688
Oleksandr Krucheniuk <bondarets1488@gmail.com>
2022-04-04 13:03:13 +0300

    finish

--- src/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.scss' {
-    const content: Record<string, string>;
-    export default content;
-   }
-   
-   declare module 'react-dom' {
-    const content: any;
-    export default content;
-   }
